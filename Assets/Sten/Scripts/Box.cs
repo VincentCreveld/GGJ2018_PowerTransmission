f496e8182b8cdd8ac3ad0516b66d04d1e62d967d
Probleemkind <sten.duindam@gmail.com>
2018-01-26 23:20:23 +0100

    updatesten

---
+++ Assets/Sten/Scripts/Box.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour, IInteractable {
+   
+    public void Act(Transform parent) {
+        Debug.Log("Im a box :)");
+        this.transform.parent = parent;
+        //UPDATE POSITION
+    }
+}
