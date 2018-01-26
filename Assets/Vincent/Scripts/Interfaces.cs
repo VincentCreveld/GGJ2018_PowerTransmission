08502ae13b8048edaf0da3c1578166a7d98ad6b8
Vincent <vincentmc99@hotmail.com>
2018-01-26 23:19:46 +0100

    Eerste commit vincent

---
+++ Assets/Vincent/Scripts/Interfaces.cs
@@ -0,0 +1,5 @@
+﻿public delegate void TransmissionEvent();
+
+public enum GameButtons {
+	a_button, b_button, x_button, y_button
+}
---
+++ Assets/Vincent/Scripts/SwapManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This class controls the transmission of powers between the two players in scene.
+public class SwapManager : MonoBehaviour {
+
+	private PlayerManager player1;
+	private PlayerManager player2;
+
+	//Enter button functionality
+	public TransmissionEvent A_ButtonTransmission;
+	//Enter button functionality
+	public TransmissionEvent B_ButtonTransmission;
+	//Enter button functionality
+	public TransmissionEvent X_ButtonTransmission;
+	//Enter button functionality
+	public TransmissionEvent Y_ButtonTransmission;
+
+	private void Start() {
+		SetupPlayerManagers();
+	}
+
+
+	private void SetupPlayerManagers() {
+		player1 = gameObject.AddComponent<PlayerManager>();
+		player2 = gameObject.AddComponent<PlayerManager>();
+
+		player1.A_ButtonSwap += A_Swap;
+		player1.B_ButtonSwap += B_Swap;
+		player1.X_ButtonSwap += X_Swap;
+		player1.Y_ButtonSwap += Y_Swap;
+
+		player2.A_ButtonSwap += A_Swap;
+		player2.B_ButtonSwap += B_Swap;
+		player2.X_ButtonSwap += X_Swap;
+		player2.Y_ButtonSwap += Y_Swap;
+
+
+	}
+
+	#region Swap Calls
+	//These functions are called with input to notify the SwapManager to switch over the controls.
+	public void A_Swap() {
+		A_ButtonTransmission();
+	}
+	public void B_Swap() {
+		B_ButtonTransmission();
+	}
+	public void X_Swap() {
+		X_ButtonTransmission();
+	}
+	public void Y_Swap() {
+		Y_ButtonTransmission();
+	}
+	#endregion
+
+}
