ca2fe777e39c84ca178f3471f6c861a5a864adc8
Probleemkind <sten.duindam@gmail.com>
2018-01-27 03:16:26 +0100

    update

---
+++ Assets/Sten/Scripts/GameOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+    public List<string> goodbyeWords = new List<string>();
+    [SerializeField]
+    private Text gameOverText;
+
+
+    public void Start() {
+        gameOverText.text = goodbyeWords[Random.Range(0, 4)];
+    }
+
+    public void FixedUpdate() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            BackToMainMenu();
+        }        
+    }
+
+    // Update is called once per frame
+    public void BackToMainMenu() {
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ Assets/Sten/Scripts/StartGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartGame : MonoBehaviour {
+
+    private bool p1IsReady = false;
+    private bool p2IsReady = false;
+
+    public void fixedUpdate() {//EDIT INPUT KEY
+        //P1
+        if (Input.GetKeyDown(KeyCode.S)) {
+            p1IsReady = true;
+            if (p1IsReady && p2IsReady) {
+                StartMainGame();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            p2IsReady = true;
+            if(p1IsReady && p2IsReady) {
+                StartMainGame();
+            }
+        }
+    }
+
+	// Update is called once per frame
+	public void StartMainGame() {
+        SceneManager.LoadScene("MainGame");
+	}
+}
