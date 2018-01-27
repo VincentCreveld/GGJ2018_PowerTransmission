cdda8d647ff25ddc296a4b357a140e7253135479
Probleemkind <sten.duindam@gmail.com>
2018-01-27 01:19:37 +0100

    Sten_Push

--- Assets/Sten/Scripts/Box.cs
+++ Assets/Sten/Scripts/Box.cs
@@ -7,6 +7,7 @@ public class Box : MonoBehaviour, IInteractable {
     public void Act(Transform parent) {
         Debug.Log("Im a box :)");
         this.transform.parent = parent;
+        //this.gameObject.GetComponent<Collider2D>().enabled = false;
         //UPDATE POSITION
     }
 }
---
+++ Assets/Sten/fakeplayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fakeplayer : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D col) {
+        //Debug.Break();
+        Debug.Log("yaya");
+        if(col.gameObject.GetComponent<IInteractable>() != null) {
+            Debug.Log("ya");
+            col.gameObject.GetComponent<IInteractable>().Act(this.transform); 
+        }
+    }
+}
