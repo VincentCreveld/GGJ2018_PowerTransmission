e6ec7be9b5c04096a4581d8eca37a1ce2c0b8121
Vincent <vincentmc99@hotmail.com>
2018-01-27 14:20:28 +0100

    StartScene multiController set up

--- Assets/Sten/Scripts/StartGame.cs
+++ Assets/Sten/Scripts/StartGame.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour {
 
+	public Transform p1UI;
+	public Transform p2UI;
+
     private bool p1IsReady = false;
     private bool p2IsReady = false;
 
-    public void fixedUpdate() {//EDIT INPUT KEY
+	public void Update() {//EDIT INPUT KEY
         //P1
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetButtonDown("A_ButtonJ1")) {
             p1IsReady = true;
-            if (p1IsReady && p2IsReady) {
+			p1UI.GetChild(0).gameObject.SetActive(true);
+			p1UI.GetChild(1).gameObject.SetActive(false);
+			if (p1IsReady && p2IsReady) {
                 StartMainGame();
             }
         }
-        if (Input.GetKeyDown(KeyCode.D)) {
+        if (Input.GetButtonDown("A_ButtonJ2")) {
             p2IsReady = true;
-            if(p1IsReady && p2IsReady) {
+			p2UI.GetChild(0).gameObject.SetActive(true);
+			p2UI.GetChild(1).gameObject.SetActive(false);
+			if(p1IsReady && p2IsReady) {
                 StartMainGame();
             }
         }
