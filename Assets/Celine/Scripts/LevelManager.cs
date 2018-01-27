287b2b0263f9970b14fb574149f1a5732237eab5
Celine de Wijs <31764854+CdeWijs@users.noreply.github.com>
2018-01-27 10:50:25 +0100

    Level Manager

---
+++ Assets/Celine/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private int level = 1;
+
+    public void Start()
+    {
+        Debug.Log("This is level " + level);
+    }
+
+    public void LevelUp()
+    {
+        level++;
+        Debug.Log("This is level " + level);
+    }
+}
---
+++ Assets/Celine/Scripts/LevelUpTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUpTrigger : MonoBehaviour
+{
+    private LevelManager levelManager;
+
+    // Boolean to check if a player has already entered the trigger
+    private bool firstEntered = false;
+
+    private void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && firstEntered == false)
+        {
+            firstEntered = true;
+        }
+        else if (collision.tag == "Player" && firstEntered == true)
+        {
+            levelManager.LevelUp();
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
+}
